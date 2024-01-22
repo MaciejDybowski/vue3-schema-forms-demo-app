8bae4b3d99bf805bb0efa35d35bcc59f4f12b1e1
Maciej <maciej.dybowski@tecna.pl>
2024-01-22 07:41:28 +0100

    add import style

--- src/App.vue
+++ src/App.vue
@@ -29,15 +29,100 @@ document.title = "Vue Schema Forms Demo App"
 const formOptions = ref({});
 const model = ref({});
 const schema = ref({
-  type: 'object',
-  properties: {
-    field1: {
-      label: 'Field 1',
-      layout: {
-        component: 'text-field',
-      },
+  "type": "object",
+  "properties": {
+    "invoiceMetadata": {
+      "properties": {
+        "pricing": {
+          "label": "The invoice is issued:",
+          "layout": {
+            "component": "radio-button",
+            "cols": 3,
+            "fillRow": true
+          },
+          "default": {
+            "value": "net",
+            "title": "at net prices",
+            "formatted": "net"
+          },
+          "source": {
+            "items": [
+              {
+                "value": "net",
+                "title": "at net prices",
+                "formatted": "net"
+              },
+              {
+                "value": "gross",
+                "title": "at gross prices",
+                "formatted": "gross"
+              }
+            ],
+            "returnObject": true
+          }
+        }
+      }
     },
-  },
+    "data": {
+      "properties": {
+        "items": {
+          "layout": {
+            "component": "duplicated-section",
+            "schema": {
+              "properties": {
+                "product": {
+                  "label": "Product",
+                  "layout": {
+                    "component": "text-field",
+                    "cols": 4
+                  }
+                },
+                "quantity": {
+                  "label": "Quantity",
+                  "type": "number",
+                  "default": 1,
+                  "layout": {
+                    "component": "text-field",
+                    "cols": 2
+                  }
+                },
+                "price": {
+                  "label": "Price ({invoiceMetadata.pricing.formatted})",
+                  "type": "number",
+                  "layout": {
+                    "component": "text-field",
+                    "cols": 3
+                  }
+                },
+                "value": {
+                  "label": "Value",
+                  "type": "number",
+                  "layout": {
+                    "component": "text-field",
+                    "cols": 3
+                  },
+                  "calculation": "quantity * price"
+                }
+              }
+            }
+          }
+        }
+      }
+    },
+    "summary": {
+      "properties": {
+        "sumValue": {
+          "label": "SUM(Value)",
+          "layout": {
+            "component": "text-field",
+            "cols": 4
+          },
+          "calculation": "SUM(value, data.items) - 300",
+          "type": "number"
+        }
+      }
+    }
+  }
 });
 
 </script>
--- src/plugins/vueSchemaForms.ts
+++ src/plugins/vueSchemaForms.ts
@@ -1,4 +1,5 @@
 import {createVueSchemaForms} from "vue3-schema-forms";
+import "vue3-schema-forms/dist/style.css"
 
 const vueSchemaForms = createVueSchemaForms()
 
